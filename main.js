fd350b3e9338d907f2202b03a37e2c0631f7707c
Szymon <szymonkrok49@gmail.com>
2024-02-24 00:52:58 +0100

    Fix new number after get result

--- main.js
+++ main.js
@@ -1,81 +1,97 @@
 // Declares all necessary variables
 
-const numbers = document.querySelectorAll('.number'),
-      operators = document.querySelectorAll('.operator'),
-      deleteBtn = document.querySelector('.delete'),
-      backBtn = document.querySelector('.back'),
-      equalsBtn = document.querySelector('.equals'),
-      previousOperation = document.querySelector('.previous-operation'),
-      currentOperation = document.querySelector('.current-operation');
+const numbers = document.querySelectorAll(".number"),
+    operators = document.querySelectorAll(".operator"),
+    deleteBtn = document.querySelector(".delete"),
+    backBtn = document.querySelector(".back"),
+    equalsBtn = document.querySelector(".equals"),
+    previousOperation = document.querySelector(".previous-operation"),
+    currentOperation = document.querySelector(".current-operation");
 
-let firstNumber = '',
-    secondNumber = '',
+let firstNumber = "",
+    secondNumber = "",
     operation = undefined;
 
 // Update result on our calculator
 const updateResult = () => {
     currentOperation.innerText = firstNumber;
-    if(operation !== undefined){
+    if (operation !== undefined) {
         previousOperation.innerText = secondNumber + operation;
     } else {
-        previousOperation.innerText = '';
+        previousOperation.innerText = "";
     }
-
-
-}
+};
 
 // Add number and check dot in number
 const addNumber = (number) => {
-    if(number === '.'){
-        if(firstNumber.includes('.')){
+    if (number === ".") {
+        if (firstNumber.includes(".")) {
             return;
         }
     }
     firstNumber = firstNumber.toString() + number.toString();
-}
+};
 
 // Possibility to select a negative number
 const chooseOperator = (operator) => {
-    if(currentOperation.innerText === ''){
-        if(operator !== "-") {
+    if (currentOperation.innerText === "") {
+        if (operator !== "-") {
             return;
         }
     }
 
-    if(previousOperation.innerText !== ''){
+    if (previousOperation.innerText !== "") {
         equals();
     }
 
     operation = operator;
     secondNumber = firstNumber;
-    firstNumber = '';
+    firstNumber = "";
+};
 
+// New number after get result
+let equalsClicked = false;
 
-}
+equalsBtn.addEventListener("click", function () {
+    equalsClicked = false;
+
+    numbers.forEach((number) => {
+        console.log(number.innerHTML);
+        number.addEventListener("click", function () {
+            if (!equalsClicked) {
+                currentOperation.innerHTML = number.innerHTML;
+                firstNumber = number.innerHTML;
+                equalsClicked = true;
+            }
+        });
+    });
+});
 
 // Main calculator functionality
 const equals = () => {
-
     let calculations;
 
-    if(!previousOperation || !currentOperation){
+    console.log(previousOperation);
+    console.log(currentOperation);
+
+    if (!previousOperation || !currentOperation) {
         return;
     }
 
     const first = parseFloat(firstNumber);
     const second = parseFloat(secondNumber);
 
-    switch (operation){
-        case '+':
+    switch (operation) {
+        case "+":
             calculations = second + first;
             break;
-        case '-':
+        case "-":
             calculations = second - first;
             break;
-        case '*':
+        case "*":
             calculations = second * first;
             break;
-        case '/':
+        case "/":
             calculations = second / first;
             break;
         default:
@@ -83,48 +99,45 @@ const equals = () => {
     }
 
     firstNumber = calculations;
-    secondNumber = '';
+    secondNumber = "";
     operation = undefined;
-
-}
+};
 
 // Operation of the AC and DEL buttons
 const removeNumber = () => {
     firstNumber = firstNumber.toString().slice(0, -1);
     updateResult();
-}
+};
 
 const deleteAll = () => {
-    firstNumber = '';
-    secondNumber = '';
-    operation = '';
+    firstNumber = "";
+    secondNumber = "";
+    operation = "";
     updateResult();
-}
+};
 
 // Operate click number and operators
 numbers.forEach((number) => {
-    number.addEventListener('click', () => {
+    number.addEventListener("click", () => {
         addNumber(number.innerText);
         updateResult();
-    })
-})
+    });
+});
 
 operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
+    operator.addEventListener("click", () => {
         chooseOperator(operator.innerText);
         updateResult();
-    })
-})
+    });
+});
 
 // Setting listener for a click
 
-backBtn.addEventListener('click', removeNumber);
+backBtn.addEventListener("click", removeNumber);
 
-deleteBtn.addEventListener('click', deleteAll);
+deleteBtn.addEventListener("click", deleteAll);
 
-equalsBtn.addEventListener('click', () => {
+equalsBtn.addEventListener("click", () => {
     equals();
     updateResult();
-} )
-
-
+});
